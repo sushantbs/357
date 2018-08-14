a0e67c0535ef3b60076d0b735d491b824f1b1c09
Sushant Badangod <sushant.bs@gmail.com>
2018-08-15 00:24:56 +0800

    Updated with snippets and the load test script

---
+++ loadtest.js
@@ -0,0 +1,51 @@
+// @ts-check
+var puppeteer = require("puppeteer");
+
+async function runLoadTest(contexts = 3) {
+  let browser = await puppeteer.launch();
+
+  let pageArr = await Promise.all(
+    Array.from(Array(contexts)).map(async (item, index) => {
+      let context = await browser.createIncognitoBrowserContext();
+      return context.newPage();
+    })
+  );
+
+  await Promise.all(
+    pageArr.map(page => {
+      return page.goto("https://ll357.herokuapp.com");
+    })
+  );
+
+  await Promise.all(
+    pageArr.map(async (page, index) => {
+      let inputHandle = await page.$("#playername");
+
+      await page.evaluateHandle(
+        (input, i) => {
+          input.value = `User${i}`;
+        },
+        inputHandle,
+        index
+      );
+
+      await page.click("#registerbutton");
+      return page.waitForNavigation({ waitUntil: "load" });
+    })
+  );
+
+  await Promise.all(
+    pageArr.map(async (page, index) => {
+      await page.click("#playbutton");
+      return page.waitForNavigation({ waitUntil: "load" });
+    })
+  );
+
+  await new Promise(resolve => setTimeout(resolve, 10000));
+
+  browser.close();
+}
+
+runLoadTest(100).then(() => {
+  process.exit(0);
+});
--- snippet.ts
+++ snippet.ts
@@ -1,3 +1,70 @@
+let a: string = "test";
+console.log(a);
+
 function testLength(a: string, b: number): boolean {
-  return true;
+  return a.length === b;
 }
+
+let b = testLength("test", 4);
+console.log(b);
+
+function vanilla(a, b) {
+  return a + b;
+}
+
+type Mammal = {
+  legs: number;
+  tail: boolean;
+  endangered: boolean;
+};
+
+let tiger: Mammal = {
+  legs: 4,
+  tail: true,
+  endangered: true
+};
+
+enum aClass {
+  "Reptile",
+  "Mammal"
+}
+
+interface Animal {
+  type: aClass;
+  isAquatic(): boolean;
+}
+
+interface MammalInterface {
+  legs: number;
+  tail: boolean;
+  endangered: boolean;
+  count(): number;
+}
+
+class AnimalClass implements Animal {
+  private aquatic: boolean;
+
+  public type: aClass;
+  public isAquatic(): boolean {
+    return this.aquatic;
+  }
+}
+
+class MammalClass extends AnimalClass implements MammalInterface {
+  private carnivore: boolean;
+
+  public legs: number;
+  public tail: boolean;
+  public endangered: boolean;
+
+  public count() {
+    return 3200;
+  }
+
+  public isCarnivore() {
+    return this.carnivore;
+  }
+}
+
+// properties and methods available on a variable is determined from the type;
+let panda = new MammalClass();
